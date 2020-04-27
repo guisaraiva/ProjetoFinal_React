26a5ed22e435acc7a24110b9aa0df4dee8df063b
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-04-26 23:37:26 -0300

    Nova alteração no projeto. Inclusão de banner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './styles.css';
+import banner from '../src/images/banner.jpg';
+
 
 function App() {
   return (
     <section class="estrutura">
-    Corpo
-    <section class="header">
-    Banner
-    </section>
-    <nav class="menu">
-    Menu
-    </nav>
-    <section class="conteudo">
-    Conteúdo
-    </section>
-    <footer class="rodape">
-    Rodapé
-    </footer>
+      <section class="header">
+            <img src={banner} alt="Banner principal"/>
+      </section>  
+      
+      <nav class="menu">
+        <ul>
+          <li>Promoções</li>
+          <li>Bolsas</li>
+          <li>Sapatos</li>
+          <li>Bolsas</li>
+        </ul>
+      
+      </nav>
+      
+      <section class="conteudo">
+      Conteúdo
+      </section>
+      
+      <footer class="rodape">
+      Rodapé
+      </footer>
     </section>
    
   );
